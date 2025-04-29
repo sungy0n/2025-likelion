3aea8655789efb773aad0fde546337a4fd6a7df8
rlatjddus123 <seongyeon0810@naver.com>
2025-04-30 04:53:41 +0900

    Tic Tac Toe

---
+++ 멋쟁이 사자처럼/week4/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "./components/section/Main";
+import TicTacToe from '../src/components/TicTacToe/TicTacToe.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App;
