6d74fea45cfe912af9db78b4f1883ae9595dbf5b
kimseongyeon <seongyeon0810@naver.com>
2025-10-09 20:38:15 +0900

    Todo리스트 추가

--- second_semster/src/component/Todo.jsx
+++ second_semster/src/component/Todo.jsx
@@ -2,20 +2,38 @@ import React from 'react'
 import { useState } from 'react'
 import '../Todo.scss'
 import Todo_Add from './Todo_Add'
-import Todo_item from './Todo_Item'
 import search_icon from '../img/search_icon.svg'
 import search1_icon from '../img/search1_icon.svg'
 import search2_icon from '../img/search2_icon.svg'
 import add_icon from '../img/add_icon.svg'
+import del_icon from '../img/del_icon.svg'
+import edit_icon from '../img/edit_icon.svg'
 
 const Todo = () => {
     // Todo_Add 팝업
     const [isVisable, setIsVisible] = useState(false);
+
     const handleOpenPopup = () =>{
         setIsVisible(true);
     }
     const handleClosePopup = () =>{
         setIsVisible(false);
+    }
+
+    //Todo 리스트
+    const [todos, setTodos] = useState([]);
+
+    const handleAddTodo = (text) => {
+        const newTodo = {
+            id: Date.now(),
+            text: text,
+        };
+        setTodos([...todos, newTodo]);
+    };
+
+    const handleDeleteTodo = (id) => {
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
     }
 
   return (
@@ -34,17 +52,22 @@ const Todo = () => {
                 <img className='search2_icon' src={search2_icon} alt="" />
             </div>
         </div>
-        <div className="todo_list_container">
-            {/* {todoList.map((item)=>(
-                <Todo_item 
-                text={item.text}
-                />
-            ))} */}
-        </div>
+        {todos.map(todo => (
+            <div className="todo_list">
+                <div className="text">
+                    <input type="checkbox" name="" id="" />
+                    <div className="todo_item">{todo.text}</div>
+                </div>
+                <div className="icon">
+                    <img src={edit_icon} alt="" />
+                    <img src={del_icon} alt="" onClick={handleDeleteTodo} />
+                </div>
+            </div>
+        ))}
         <div className="add" onClick={handleOpenPopup}>
             <img className='add_icon' src={add_icon} alt="" />
         </div>
-        {isVisable && <Todo_Add onclose={handleClosePopup}/>}
+        {isVisable && <Todo_Add onclose={handleClosePopup} onAdd={handleAddTodo}/>}
     </div>
   )
 }
--- second_semster/src/component/Todo_Add.jsx
+++ second_semster/src/component/Todo_Add.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import { useState } from 'react'
 import '../Todo.scss'
 
-const Todo_add = ({onclose}) => {
+const Todo_add = ({onclose, onAdd}) => {
+
+  const [inputText, setInputText] = useState('');
+
+  const handleApplyClikk = () => {
+    if(inputText.trim() === ''){
+        alert('내용을 입력해주세요')  
+        return;
+    }
+    onAdd(inputText);
+    onclose();
+  }; 
   
   return (
     <div id="Wrap_Todo_add">
         <h1>NEW NOTE</h1>
         <div className="add_input">
-          <input type="text" placeholder='Input your note...' />
+          <input type="text" placeholder='Input your note...' value={inputText} onChange={(e)=>setInputText(e.target.value)} />
         </div>
         <div className="btn">
             <div className="cancel" onClick={onclose}>CANCEL</div>
-           <div className="apply">APPLY</div>
+           <div className="apply" onClick={handleApplyClikk} >APPLY</div>
         </div>     
     </div>
   )
